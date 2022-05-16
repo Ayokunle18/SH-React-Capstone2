e213e450d19c78a23c2955e5315af0533f606ce2
abrhame <ablakew7ab@gmail.com>
2022-05-17 00:33:34 +0300

    team members

--- src/Pages/Team/Team.js
+++ src/Pages/Team/Team.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 
-const Team = () => {
-  return (
-    <div>
-      <Sidebar/>
-      Team</div>
-  )
+import './team.css'
+// Example of a data array that
+// you might receive from an API
+const data = [
+{ name: "Abrham", age: 19, gender: "Male" },
+{ name: "Ayo", age: 19, gender: "Female" },
+{ name: "Subham", age: 25, gender: "Male"},
+]
+
+function App() {
+return (
+  <>
+  <Sidebar />
+	<div className="App">
+	<table>
+		<tr>
+		<th>Name</th>
+		<th>Age</th>
+		<th>Gender</th>
+		</tr>
+		{data.map((val, key) => {
+		return (
+			<tr key={key}>
+			<td>{val.name}</td>
+			<td>{val.age}</td>
+			<td>{val.gender}</td>
+			</tr>
+		)
+		})}
+	</table>
+	</div>
+  </>
+);
 }
 
-export default Team
+export default App;
+
+
